021e933fe5cb322dc78676a438771a69db7d112a
imprantu <rakinhasanbabu4@gmail.com>
2021-11-11 13:59:42 +0600

    updated product name

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.whfic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -13,16 +17,16 @@ async function run() {
         await client.connect();
 
         const database = client.db('flowers-valley');
-        const roseCollection = database.collection('roses');
+        const bouquetsCollection = database.collection('bouquets');
         const occasionCollection = database.collection('occasion');
 
-        // GET API : Roses
-        app.get('/roses', async (req, res) => {
-            const roses = await roseCollection.find({}).toArray();
-            res.json(roses);
+        // GET API : Rose Bouquets
+        app.get('/bouquets', async (req, res) => {
+            const bouquets = await bouquetsCollection.find({}).toArray();
+            res.json(bouquets);
         });
 
-        // GET API : Collection
+        // GET API : Occasion
         app.get('/occasion', async (req, res) => {
             const occasion = await occasionCollection.find({}).toArray();
             res.json(occasion);
